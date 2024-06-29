b89ad9f6cbe541d5cc278caa8e17f632aaed5776
행복한엄쌤 <sendmethere@naver.com>
2024-06-29 14:29:54 +0900

    1.10

--- src/Report.js
+++ src/Report.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { toPng } from 'html-to-image';
+import { saveAs } from 'file-saver';
+
 import useStore from './store'
 
 const Report = () => {
-    const { topic, independentVariable, dependentVariable, method, data, dataCollection, subject, consideration, setConsideration } = useStore();
+    const { menu, topic, independentVariable, dependentVariable, method, data, dataCollection, subject, consideration, setConsideration } = useStore();
+    const reportRef = useRef(null);
 
     const handleConsiderationChange = (event) => {
         setConsideration(event.target.value);
     };
 
+    const downloadImage = () => {
+        if (reportRef.current === null) {
+          return;
+        }
+    
+        toPng(reportRef.current, { cacheBust: true })
+          .then((dataUrl) => {
+            saveAs(dataUrl, 'report.png');
+          })
+          .catch((err) => {
+            console.error('Oops, something went wrong!', err);
+          });
+      };
+
     return (
         <div>
+        <div className='bg-white p-2' ref={reportRef}>
             <div className='mb-4 text-xl font-bold py-4 rounded-xl cursor-pointer hover:bg-gray-100 transition-all duration-200 hover:scale-[1.02] hover:-translate-y-1 '>
             {topic !== "" ? <p className='text-center'> {topic}</p> : <p className='text-gray-400 text-center'>연구 주제를 입력해주세요</p> }
             </div>
@@ -53,6 +72,16 @@ const Report = () => {
                     rows={4}
                 />
             </div>
+        </div>
+        <div className='flex justify-end'>
+            <button
+                onClick={downloadImage}
+                style={{backgroundColor: menu[5].color}}
+                className='mt-4 text-white p-2 rounded-xl hover:brightness-110 transition-all duration-200 hover:scale-[1.02]'
+            >
+                이미지로 저장
+            </button>
+        </div>
         </div>
     )
 }
